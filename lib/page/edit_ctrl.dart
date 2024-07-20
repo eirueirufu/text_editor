e257539032dd27a8755b3ec4920cd8167630ae78
eirueirufu <57451255+eirueirufu@users.noreply.github.com>
2024-07-20 12:10:29 +0800

    crdt文本同步

--- lib/page/edit_ctrl.dart
+++ lib/page/edit_ctrl.dart
@@ -2,14 +2,17 @@ import 'package:flutter/services.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_quill/quill_delta.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:text_editor/model/book.dart';
 import 'package:text_editor/service/crdt.dart';
 import 'package:text_editor/service/db.dart';
+import 'package:text_editor/service/sp.dart';
 import 'package:y_crdt/wit_world.dart';
 
 class EditController extends GetxController {
   final dbService = Get.find<DbService>();
   final crdtService = Get.find<CrdtService>();
+  final spService = Get.find<SpService>();
 
   final int id;
   late Book book;
@@ -19,11 +22,20 @@ class EditController extends GetxController {
   late YDoc crdtDoc;
   late YText crdtText;
 
+  late RealtimeChannel chan;
+
   EditController({required this.id});
 
   @override
   void onInit() async {
     super.onInit();
+    chan = spService.supabase.channel(
+      "test",
+      opts: const RealtimeChannelConfig(
+        self: true,
+      ),
+    );
+
     fetchText();
 
     doc = Document();
@@ -55,8 +67,31 @@ class EditController extends GetxController {
       final ops = event.change.operations;
       crdtService.operationsApplyToText(crdtText, ops);
 
-      print(crdtService.crdtTextToOperations(crdtText));
+      final state =
+          crdtService.world.yDocMethods.encodeStateAsUpdate(ref: crdtDoc).ok;
+
+      chan.sendBroadcastMessage(
+          event: "book_1", payload: Map()..["state"] = state);
     });
+
+    chan
+        .onBroadcast(
+            event: "book_1",
+            callback: (payload) {
+              final origin = crdtService.world.yDocMethods
+                  .encodeStateAsUpdate(ref: crdtDoc)
+                  .ok!;
+              final state = (payload)["state"];
+              if (state == null) {
+                return;
+              }
+              final update = Uint8List.fromList(
+                  (state as List<dynamic>).map((e) => e as int).toList());
+
+              crdtService.world.yDocMethods
+                  .applyUpdate(ref: crdtDoc, diff: update, origin: origin);
+            })
+        .subscribe();
   }
 
   @override
